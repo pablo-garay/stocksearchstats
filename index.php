fe049108172e503ac6948c9e6c65950b5f14379b
Pablo Garay <pgaray@roshka.com>
2016-03-23 10:35:42 -0700

    Included carousel to index. Fixed scripts inclusion: bootstrap, jquery, js scripts

--- index.php
+++ index.php
@@ -2,174 +2,194 @@
 <html>
 
 <head>
-<style>
-	body {
-		text-align: left;
-		margin: 0;
-	}
-	div.centered 
-	{
-	    text-align: center;
-	    padding: 4em;
-	}
-
-	div.centered table 
-	{
-	    margin: 0 auto; 
-	}
-
-	h1 {
-		margin: 0; padding: 0;
-	}
-
-	div.form-container {
-		background-color: #F5F5F5;
-		border: 1px solid;
-		border-color: #DCDCDC;
-		padding-top: 5px;
-		padding-bottom: 2em;
-		width: 700px;
-		height: 125px;
-		margin: 0 auto;
-		display: block;
-		margin-top: 10px; margin-bottom: 10px;
-	}
-
-	hr {
-		background:#CDCCCD; 
-		border:0; 
-		height:1px;
-		width:97%;
-		margin: 0 auto; padding: 0;
-	}
-
-	.form-blocks {
-		display: block;
-	}
-
-	.form-block{
-		margin-top: 5px;
-		padding: 0;
-	}
-
-	.form-input {
-		margin-top: 20px;
-		margin-left: 10px;
-		text-align: left;
-	}
-
-	.form-title {
-		font-weight: bold;
-		font-style: italic;
-		margin: 0; padding: 0;
-	}
-
-	.box {
-		display:inline-block;
-		vertical-align:top;
-	}
-
-	.inlined {
-		display: inline;
-	}
-
-	input[type=submit], input[type=button] {
-		background-color: white;
-		border-radius:5px;
-		border: solid #CCCBCB;
-		border-width: 1px;
-	}
-
-	input[type=text]{
-		border: solid #CCCBCB;
-		border-width: 1px;
-        text-align: left;
-	}
-
-
-	table, th, td {
-	    border-collapse: collapse;
-	    font-family: Arial, Helvetica, sans-serif;
-	}
-	th, td {
-		text-align: left;
-	    padding: 5px;
-	}
-	th {
-		font-weight: bold;
-		text-align: left;
-		background-color: #F5F5F5;
-	}
-	td {
-		background-color: #FBFAF9;
-	}
-	td.td2 {
-		text-align: center;
-	}
-
-	.table1, .table2{
-		width: 710px;
-	}
-
-	.table2 {
-		height: 432px;
-	}
-
-	/* table inner borders */
-	td + td,
-	th + th,
-	th + td { border-left: 1px solid #CCCBCB; }
-	tr + tr { border-top: 1px solid #CCCBCB; }
-
-	/* table outer borders */
-	/* up */
-	table tr:first-child{
-	  border-top: solid 2px;
-	  border-color: #CCCBCB;
-	}
-	/* down */
-	table tr:last-child{
-	  border-bottom: solid 2px;
-	  border-color: #CCCBCB;
-	}
-	/* left */
-	table td:first-child,
-	table th:first-child {
-	  border-left: solid 2px;
-	  border-color: #CCCBCB;
-	}
-	/* right */
-	table td:last-child,
-	table th:last-child {
-	  border-right: solid 2px;
-	  border-color: #CCCBCB;
-	}
     
-    .markit-on-demand-logo {
-        height: 20px;
-		width: 100px;
-    }
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap Carousel Example - Bootstrap 101 Template</title>
+    
+    <style>
+        body {
+            text-align: left;
+            margin: 0;
+        }
+        div.centered 
+        {
+            text-align: center;
+            padding: 4em;
+        }
 
-	.marker-icon {
-		height: 12px;
-		width: 12px;
-	}
+        div.centered table 
+        {
+            margin: 0 auto; 
+        }
 
-	.info-message {
-		background-color: #FCFBFA;
-		border: 2px solid;
-	    border-color: #CCCBCB;
-	    margin: 0 auto;
-	    padding: 4px;
-	    width: 700px;
-	    font-family: Arial, Helvetica, sans-serif;
-	    text-align: center;
-	}
+        h1 {
+            margin: 0; padding: 0;
+        }
 
-</style>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.2.min.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+        div.form-container {
+            background-color: #F5F5F5;
+            border: 1px solid;
+            border-color: #DCDCDC;
+            padding-top: 5px;
+            padding-bottom: 2em;
+            width: 700px;
+            height: 125px;
+            margin: 0 auto;
+            display: block;
+            margin-top: 10px; margin-bottom: 10px;
+        }
+
+        hr {
+            background:#CDCCCD; 
+            border:0; 
+            height:1px;
+            width:97%;
+            margin: 0 auto; padding: 0;
+        }
+
+        .form-blocks {
+            display: block;
+        }
+
+        .form-block{
+            margin-top: 5px;
+            padding: 0;
+        }
+
+        .form-input {
+            margin-top: 20px;
+            margin-left: 10px;
+            text-align: left;
+        }
+
+        .form-title {
+            font-weight: bold;
+            font-style: italic;
+            margin: 0; padding: 0;
+        }
+
+        .box {
+            display:inline-block;
+            vertical-align:top;
+        }
+
+        .inlined {
+            display: inline;
+        }
+
+        input[type=submit], input[type=button] {
+            background-color: white;
+            border-radius:5px;
+            border: solid #CCCBCB;
+            border-width: 1px;
+        }
+
+        input[type=text]{
+            border: solid #CCCBCB;
+            border-width: 1px;
+            text-align: left;
+        }
+
+
+        table, th, td {
+            border-collapse: collapse;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        th, td {
+            text-align: left;
+            padding: 5px;
+        }
+        th {
+            font-weight: bold;
+            text-align: left;
+            background-color: #F5F5F5;
+        }
+        td {
+            background-color: #FBFAF9;
+        }
+        td.td2 {
+            text-align: center;
+        }
+
+        .table1, .table2{
+            width: 710px;
+        }
+
+        .table2 {
+            height: 432px;
+        }
+
+        /* table inner borders */
+        td + td,
+        th + th,
+        th + td { border-left: 1px solid #CCCBCB; }
+        tr + tr { border-top: 1px solid #CCCBCB; }
+
+        /* table outer borders */
+        /* up */
+        table tr:first-child{
+          border-top: solid 2px;
+          border-color: #CCCBCB;
+        }
+        /* down */
+        table tr:last-child{
+          border-bottom: solid 2px;
+          border-color: #CCCBCB;
+        }
+        /* left */
+        table td:first-child,
+        table th:first-child {
+          border-left: solid 2px;
+          border-color: #CCCBCB;
+        }
+        /* right */
+        table td:last-child,
+        table th:last-child {
+          border-right: solid 2px;
+          border-color: #CCCBCB;
+        }
+
+        .markit-on-demand-logo {
+            height: 20px;
+            width: 100px;
+        }
+
+        .marker-icon {
+            height: 12px;
+            width: 12px;
+        }
+
+        .info-message {
+            background-color: #FCFBFA;
+            border: 2px solid;
+            border-color: #CCCBCB;
+            margin: 0 auto;
+            padding: 4px;
+            width: 700px;
+            font-family: Arial, Helvetica, sans-serif;
+            text-align: center;
+        }
+
+    </style>
+    
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->      
+      
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+    
+    <!-- Clear button's logic -->
     <script type="text/javascript">
         function clearSearch(){
             document.getElementById("input").value="";
@@ -189,7 +209,7 @@
 					<div id="content">				
                         <div class="box">
 						<label for="input">Enter the stock name or symbol*: </label>						
-							<input type="text" id="input" name="input" placeholder="e.g. Apple"
+							<input type="text" id="input" name="input" placeholder="e.g. Apple Inc or AAPL"
 								required pattern="^[a-zA-Z0-9][a-zA-Z0-9 ]*$" 
 								value='<?php if (isset($_GET["input"])) echo htmlspecialchars($_GET["input"]); ?>' > </input>
                                 <div class="box">
@@ -204,6 +224,47 @@
 				</div>
 			</form>
         </div>
+
+        <!-- Carousel
+        ================================================== -->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+          <!-- Indicators (indicates with circle image below which slide is currently shown) -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Favorite List</h1>
+                  <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Stock Details</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Left and right buttons -->
+          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div><!-- /.carousel -->
 
         <div id="resultsArea">
 			<?php
@@ -335,7 +396,7 @@
 				    } else {
 						if ($xmlElement->count() == 0) {
 					    //it's empty
-					    echo '<div class="info-message">No Record has been found</div>';
+					    echo '<div class="info-message">Select a valid entry</div>';
 					    
 					    // print_r($xmlElement);
 					}
@@ -365,8 +426,28 @@
 				}
 			?>
 		</div>
-	</div>
-<script>
+	</div>    
+
+    <!-- ============= JS scripts ============= -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- include jquery libraries -->
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.2.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>      
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!--    <script src="js/bootstrap.min.js"></script>-->
+
+    <script>
     // A $( document ).ready() block.
     $( document ).ready(function() {
         console.log( "ready!" );
@@ -412,7 +493,7 @@
         });
     });
     
-</script>
-<noscript>
+    </script>
+    <noscript>
 </body>
 </html>
